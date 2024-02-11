f040062e27d83e12c70a63a82f653c02d2db642c
SlavaSid <kilory@mail.ru>
2024-02-11 23:19:30 +0300

    add Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home.jsx";
 import { Game } from "./components/Game/Game.jsx";
 import { NotFound } from "./components/NotFound/NotFound.jsx";
+import { Layout } from "./components/Layout.jsx";
 
 function App() {
   return (
     <div className="App">
        <Routes>
+       <Route path="/" element={<Layout />}>
       <Route path="/" element={<Home />} />
       <Route path="/:provider/:game" element={<Game />} />
       <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
     </div>
   );
